a3ef09fd0b1dad6997f6cbb7d309f84b0e6f8c82
Pichborith Kong <pichborith@gmail.com>
2023-05-06 19:08:57 -0700

    add middleware to validate request

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import controller from '../controllers/Author';
+import { Schemas, ValidateJoi } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', controller.createAuthor);
+router.post(
+  '/create',
+  ValidateJoi(Schemas.author.create),
+  controller.createAuthor
+);
 router.get('/get', controller.readAll);
 router.get('/get/:authorId', controller.readAuthor);
-router.patch('/update/:authorId', controller.updateAuthor);
+router.patch(
+  '/update/:authorId',
+  ValidateJoi(Schemas.author.update),
+  controller.updateAuthor
+);
 router.delete('/delete/:authorId', controller.deleteAuthor);
 
 export = router;
