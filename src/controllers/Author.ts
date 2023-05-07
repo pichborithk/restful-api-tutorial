730caf1f19e2685db592547366a9b5cf0d1442a7
Pichborith Kong <pichborith@gmail.com>
2023-05-06 18:18:43 -0700

    add author CRUD controller and router

---
+++ src/controllers/Author.ts
@@ -0,0 +1,81 @@
+import { NextFunction, Request, Response } from 'express';
+import Author from '../models/Author';
+import mongoose from 'mongoose';
+
+const createAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name } = req.body;
+
+  const author = new Author({
+    _id: new mongoose.Types.ObjectId(),
+    name,
+  });
+
+  return author
+    .save()
+    .then(author => res.status(201).json({ author }))
+    .catch(error => res.status(500).json({ error }));
+};
+const readAuthor = async (req: Request, res: Response, next: NextFunction) => {
+  const authorId = req.params.authorId;
+
+  return Author.findById(authorId)
+    .then(author =>
+      author
+        ? res.status(200).json({ author })
+        : res.status(404).json({ message: 'Not found' })
+    )
+    .catch(error => res.status(500).json({ error }));
+};
+const readAll = async (req: Request, res: Response, next: NextFunction) => {
+  return Author.find()
+    .then(authors => res.status(200).json({ authors }))
+    .catch(error => res.status(500).json({ error }));
+};
+const updateAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorId = req.params.authorId;
+
+  return Author.findById(authorId)
+    .then(author => {
+      if (author) {
+        author.set(req.body);
+        return author
+          .save()
+          .then(author => res.status(201).json({ author }))
+          .catch(error => res.status(500).json({ error }));
+      } else {
+        res.status(404).json({ message: 'Not found' });
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+const deleteAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorId = req.params.authorId;
+
+  return Author.findByIdAndDelete(authorId)
+    .then(author =>
+      author
+        ? res.status(201).json({ message: 'Deleted' })
+        : res.status(404).json({ message: 'Not found' })
+    )
+    .catch(error => res.status(500).json({ error }));
+};
+
+export default {
+  createAuthor,
+  readAuthor,
+  readAll,
+  updateAuthor,
+  deleteAuthor,
+};
